895fb2b0cddd8fd44f8bb5126cdb85b475ccf8b4
龚裕磊 <yl_gong@droidhang.com>
2021-01-23 15:19:06 +0800

    add note

---
+++ LeetCode/linked_list/21_test.go
@@ -0,0 +1,60 @@
+package linked_list
+
+import (
+	"fmt"
+	"testing"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	prehead := &ListNode{}
+	result := prehead
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			prehead.Next = l1
+			l1 = l1.Next
+		} else {
+			prehead.Next = l2
+			l2 = l2.Next
+		}
+		prehead = prehead.Next
+	}
+	if l1 != nil {
+		prehead.Next = l1
+	}
+	if l2 != nil {
+		prehead.Next = l2
+	}
+	return result.Next
+}
+
+func NewLinkedList(nums []int) (head *ListNode) {
+	head = &ListNode{Val: nums[0], Next: nil}
+	l := head
+	for i, val := range nums {
+		if i == 0 {
+			continue
+		}
+		tmp := &ListNode{Val: val, Next: nil}
+		head.Next = tmp
+		head = head.Next
+	}
+	return l
+}
+
+func printList(head *ListNode) {
+	cur := head
+	for cur != nil {
+		fmt.Println(cur.Val)
+		cur = cur.Next
+	}
+}
+func TestMergeTwoLists(t *testing.T) {
+	l1 := NewLinkedList([]int{1, 2, 4})
+	l2 := NewLinkedList([]int{1, 3, 4})
+	printList(mergeTwoLists(l1, l2))
+}
