6f7fa9688d5817076318e53aae7f4f58d593e53f
龚裕磊 <yl_gong@droidhang.com>
2021-01-24 21:59:08 +0800

    add note

---
+++ LeetCode/array/88_3_test.go
@@ -0,0 +1,37 @@
+package array
+
+import "testing"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	p := m + n
+	for m > 0 && n > 0 {
+		if nums1[m-1] < nums2[n-1] {
+			nums1[p-1] = nums2[n-1]
+			n--
+		} else {
+			nums1[p-1] = nums1[m-1]
+			m--
+		}
+		p--
+	}
+	for i := 0; i < n; i++ {
+		nums1[i] = nums2[i]
+	}
+
+}
+
+func TestMerge(t *testing.T) {
+	/*
+		nums1 := []int{0}
+		m := 0
+		nums2 := []int{1}
+		n := 1
+	*/
+	nums1 := []int{1, 2, 3, 0, 0, 0}
+	m := 3
+	nums2 := []int{2, 5, 6}
+	n := 3
+
+	merge(nums1, m, nums2, n)
+	t.Log(nums1)
+}
