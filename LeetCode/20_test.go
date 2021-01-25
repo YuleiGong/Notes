f3cced3633eb357a97575ae35a89914468480fed
龚裕磊 <yl_gong@droidhang.com>
2021-01-25 21:45:57 +0800

    add note

---
+++ LeetCode/20_test.go
@@ -0,0 +1,34 @@
+package stack
+
+import (
+	"testing"
+)
+
+func isValid(s string) bool {
+	if s == "" {
+		return true
+	}
+	hash := map[byte]byte{
+		'}': '{',
+		']': '[',
+		')': '(',
+	}
+	stack := []byte{}
+	for _, c := range []byte(s) {
+		if c == '(' || c == '[' || c == '{' {
+			stack = append(stack, c)
+		} else if len(stack) > 0 && stack[len(stack)-1] == hash[c] {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	return len(stack) == 0
+
+}
+
+func TestIsValid(t *testing.T) {
+	a := "(())"
+	t.Log(isValid(a))
+
+}
