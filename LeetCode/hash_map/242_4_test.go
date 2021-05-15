e94e1bd2a9b1f8a90153a9cb6ad8d482e4042eba
龚裕磊 <yl_gong@droidhang.com>
2021-05-15 14:09:55 +0800

    add note

---
+++ LeetCode/hash_map/242_4_test.go
@@ -0,0 +1,29 @@
+package main
+
+import "testing"
+
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+
+	hash := make(map[byte]int64)
+	for _, c := range []byte(s) {
+		hash[c]++
+	}
+	for _, c := range []byte(t) {
+		hash[c]--
+		if hash[c] < 0 {
+			return false
+		}
+	}
+
+	return true
+
+}
+
+func TestIsAnagram(t *testing.T) {
+	s := "rat"
+	t1 := "car"
+	t.Log(isAnagram(s, t1))
+}
