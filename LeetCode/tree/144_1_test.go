93d421d3b9aac17fcedb8d5ebea29326530fc4d8
龚裕磊 <yl_gong@droidhang.com>
2021-05-15 14:53:36 +0800

    add note

---
+++ LeetCode/tree/144_1_test.go
@@ -0,0 +1,39 @@
+package tree
+
+import "testing"
+
+//二叉树的前序遍历
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func preorderTraversal(root *TreeNode) []int {
+	var vals []int
+	var preorder func(*TreeNode)
+
+	preorder = func(node *TreeNode) {
+		if node == nil {
+			return
+		}
+		vals = append(vals, node.Val)
+		preorder(node.Left)
+		preorder(node.Right)
+	}
+	preorder(root)
+
+	return vals
+}
+
+func TestPreorderTraversal(t *testing.T) {
+	root := &TreeNode{
+		Val: 1,
+	}
+	root.Left = &TreeNode{Val: 4}
+	root.Right = &TreeNode{Val: 2}
+	root.Right.Left = &TreeNode{Val: 3}
+	root.Right.Right = &TreeNode{Val: 5}
+	t.Log(preorderTraversal(root))
+}
