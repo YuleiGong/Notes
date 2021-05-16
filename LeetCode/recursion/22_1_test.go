9e7058f0830ad9fb23f889f32452137b8c24f71d
龚裕磊 <yl_gong@droidhang.com>
2021-05-16 21:22:59 +0800

    add notes

---
+++ LeetCode/recursion/22_1_test.go
@@ -0,0 +1,12 @@
+package recursion
+
+import "testing"
+
+func generateParenthesis(n int) []string {
+	var result []string
+
+}
+
+func TestGenerateParenthesis(t *testing.T) {
+	t.Log(generateParenthesis(3))
+}
