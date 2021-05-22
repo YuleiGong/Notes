9b907d81205fadf5251e4936154fe02522439bd2
龚裕磊 <yl_gong@droidhang.com>
2021-05-22 10:23:45 +0800

    add notes

---
+++ LeetCode/recursion/226_1_test.go
@@ -0,0 +1,35 @@
+package recursion
+
+import "testing"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	left := invertTree(root.Left)
+	right := invertTree(root.Right)
+	root.Left = right
+	root.Right = left
+
+	return root
+
+}
+
+func TestInvertTree(t *testing.T) {
+	root := &TreeNode{Val: 4}
+	root.Left = &TreeNode{Val: 2}
+	root.Left.Left = &TreeNode{Val: 1}
+	root.Left.Right = &TreeNode{Val: 3}
+
+	root.Right = &TreeNode{Val: 7}
+	root.Right.Left = &TreeNode{Val: 6}
+	root.Right.Right = &TreeNode{Val: 9}
+
+	invertTree(root)
+}
