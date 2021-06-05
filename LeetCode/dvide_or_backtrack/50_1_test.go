fade804623e173a9f294f068b7b5b1e0c59275f9
龚裕磊 <yl_gong@droidhang.com>
2021-06-05 18:44:33 +0800

    add note

--- LeetCode/dvide_or_backtrack/50_1_test.go
+++ LeetCode/dvide_or_backtrack/50_1_test.go
@@ -3,8 +3,25 @@ package dvide_or_backtrack
 import "testing"
 
 func myPow(x float64, n int) float64 {
+	var helper func(n int) float64
+
+	helper = func(n int) float64 {
+		if n == 0 {
+			return 1
+		}
+		val := helper(n / 2)
+		if n%2 == 0 {
+			return val * val
+		}
+		return val * val * x //奇数
+	}
+	if n >= 0 {
+		return helper(n)
+	}
+	return 1 / helper(-n)
 
 }
 
 func TestMyPow(t *testing.T) {
+	t.Log(myPow(2, 3))
 }
