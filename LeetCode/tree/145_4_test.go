6b1b82ce0e0da8aa4b8c8dcdae35d75f8b7ce119
龚裕磊 <yl_gong@droidhang.com>
2021-06-28 22:27:28 +0800

    add notes

---
+++ LeetCode/tree/145_4_test.go
@@ -0,0 +1,38 @@
+package tree
+
+import "testing"
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	var result []int
+	var helper func(root *TreeNode)
+
+	helper = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		helper(root.Left)
+		helper(root.Right)
+		result = append(result, root.Val)
+	}
+	helper(root)
+
+	return result
+}
+
+func TestPostorderTraversal(t *testing.T) {
+	root := &TreeNode{
+		Val: 1,
+	}
+	root.Left = &TreeNode{Val: 4}
+	root.Right = &TreeNode{Val: 2}
+	root.Right.Left = &TreeNode{Val: 3}
+	root.Right.Right = &TreeNode{Val: 5}
+	t.Log(postorderTraversal(root))
+}
